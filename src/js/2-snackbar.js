6c9c1810323e8a7690c7b5a24ff77f102619e916
dkaliazin <dkaliazin@gmail.com>
2024-05-26 10:09:00 +0300

    123

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,36 @@
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+const form = document.querySelector(".form");
+function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+}
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const delay = parseInt(event.target.delay.value);
+    const state = event.target.state.value;
+
+    createPromise(delay, state)
+        .then(delay => {
+            iziToast.success({
+                title: 'Success',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch(delay => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+            });
+        });
+});
+
