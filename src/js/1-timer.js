64cd4118020d1bb0b2916182cc77548ae41ab685
dkaliazin <dkaliazin@gmail.com>
2024-05-26 09:19:48 +0300

    123

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,7 +5,7 @@ import "izitoast/dist/css/iziToast.min.css";
 const datetimePicker = document.getElementById('datetime-picker');
 const startButton = document.querySelector('button[data-start]');
 startButton.disabled = true;
-const timerDisplay = document.getElementById('timer');
+const timerDisplay = document.querySelector('timer');
 let userSelectedDate;
 let countdownInterval;
 const options = {
@@ -16,7 +16,10 @@ const options = {
    onClose(selectedDates) {
     const selectedDate = selectedDates[0];
     if (selectedDate <= new Date()) {
-      alert("Please choose a date in the future");
+        iziToast.error({
+                title: "Error",
+                message: "Please choose a date in the future",
+            });
       startButton.disabled = true;
     } else {
         userSelectedDate = selectedDate;
@@ -29,8 +32,7 @@ flatpickr(datetimePicker, options);
   startButton.addEventListener('click', () => {
     if (userSelectedDate) {
       startCountdown(userSelectedDate);
-      //startButton.disabled = true;
-      //datetimePicker.disabled = true;
+      startButton.disabled = true;
     }
   });
 
@@ -62,12 +64,10 @@ flatpickr(datetimePicker, options);
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-
     const days = Math.floor(ms / day);
     const hours = Math.floor((ms % day) / hour);
     const minutes = Math.floor((ms % hour) / minute);
     const seconds = Math.floor((ms % minute) / second);
-
     return { days, hours, minutes, seconds };
   }
 const timeToUpdate = {
